3b97bfe7b9d68def6d5c16e80a1ecbdf60c00101
Daniel Kloosterman <buz.i286@gmail.com>
2014-10-21 15:16:12 +0200

    Update install.js

--- install.js
+++ install.js
@@ -274,7 +274,7 @@ function extractDownload(filePath) {
 
   } else {
     console.log('Extracting tar contents (via spawned process)')
-    cp.execFile('tar', ['jxf', filePath], options, function (err, stdout, stderr) {
+    cp.execFile('tar', ['xf', filePath], options, function (err, stdout, stderr) {
       if (err) {
         console.error('Error extracting archive')
         deferred.reject(err)
