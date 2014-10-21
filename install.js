2a764061a9ef13fd7d66d8302fa6cd8677774342
Daniel Kloosterman <buz.i286@gmail.com>
2014-10-21 15:43:37 +0200

    Update install.js

--- install.js
+++ install.js
@@ -279,7 +279,7 @@ function extractDownload(filePath) {
         console.error('Error extracting archive')
         deferred.reject(err)
       } else {
-        fs.chmodSync(extractedPath +'/phantomjs', '0777')
+        fs.chmodSync(extractedPath +'/bin/phantomjs', '0777')
         deferred.resolve(extractedPath)
       }
     })
